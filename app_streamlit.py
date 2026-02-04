9f8b50d563d74d570698d0c1ed5def9124d04cb8
FarelRamadhan799 <ramadhanfarel079akun@gmail.com>
2026-02-04 10:42:18 +0700

    Update app_streamlit.py

--- app_streamlit.py
+++ app_streamlit.py
@@ -4,11 +4,6 @@ import streamlit as st
 
 model = joblib.load("model_random_forest.joblib")
 
-set_page_config (
-	page_title("Regresi Tomat"),
-	page_icon(":tomato:")
-)
-
 st.sidebar.title(":tomato: Machine Learning")
 st.sidebar.success("Dibuat oleh Farel")
 
@@ -26,5 +21,6 @@ if st.button("Pediksi"):
 	st.success(f"Model memprediksi total penjualan tomat {prediksi:.0f}")
 
 	st.balloons()
+
 
 
